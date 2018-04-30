701fb4702f7b247df89ad8d0a094f70edb5b1f27
Marcus Dantas <mpsdantas15@gmail.com>
2018-04-30 17:12:58 -0300

    fix(src/controllers/errorsController/): Ajuste de bug quando não era enviado nenhum arquivo.

--- src/controllers/errorsController/getErrorsFile.js
+++ src/controllers/errorsController/getErrorsFile.js
@@ -1,7 +1,12 @@
 const path = require('path');
 exports.getErrorsFile = (req) =>{
+    if(!req.files){
+        console.log('Entrou')
+        return {statusErroFile:true,msg:`Você deve anexar algum arquivo`};
+    }
     const sampleFile = req.files.arquivo;
-    if(sampleFile==undefined || path.extname(sampleFile.name)!=='.stl' || path.extname(sampleFile.name)!=='.zip' || path.extname(sampleFile.name)!=='.gbr'){
+    if(!(path.extname(sampleFile.name)==='.stl' || path.extname(sampleFile.name)==='.zip' || path.extname(sampleFile.name)==='.gbr')){
         return {statusErroFile:true,msg:`Você não pode enviar arquivos com a extensão ${path.extname(sampleFile.name)}`};
     }
+    return {statusErroFile:false,msg:`Envio de arquivo autorizado.`};
 };

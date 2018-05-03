51b22478817875300a79a4ca59eca7539e24a27a
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-03 19:10:40 -0300

    feat(src/routes/public/orcamento): Rotas responsáveis por orcamentos

--- src/routes/public/orcamento/orcamento.js
+++ src/routes/public/orcamento/orcamento.js
@@ -1,3 +1,5 @@
+const orcamentosController = require('../../../controllers/orcamentosControllers');
 module.exports = (application) => {
-    application.get('/novo-orcamento', (req, res) => { res.render('public/novo-orcamento') });
+    application.get('/novo-orcamento', (req, res) => { res.render('public/novo-orcamento')});
+    application.post('/novo-orcamento', (req, res) => { orcamentosController.criarOrcamento(application, req, res)});
 }

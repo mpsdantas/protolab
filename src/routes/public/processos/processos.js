cc3f37630aaafeb69a3954d91414ec7d5818c262
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-04 18:26:17 -0300

    feat(src/routes/): Criação de rotas de orçamento.

--- src/routes/public/processos/processos.js
+++ src/routes/public/processos/processos.js
@@ -1,5 +1,6 @@
 const processosController = require('../../../controllers/processosController');
 module.exports = (application) => {
-    application.get('/novo-processo', (req, res) => {res.render('public/novo-processo')});
-    application.post('/novo-processo', (req, res) => {processosController.criarProcesso(req, res);});
+    application.get('/novo-processo', (req, res) => { res.render('public/novo-processo') });
+    application.post('/novo-processo', (req, res) => { processosController.criarProcesso(req, res); });
+    application.get('/buscar-processo/:id', (req, res) => { processosController.pesquisarProcesso(application, req, res) });
 }

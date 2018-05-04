03694425ee2bc06c4b642a81cb2f90f8896659b3
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-04 15:08:21 -0300

    feat(src/models): Novo model de Orçamentos

---
+++ src/models/Orcamento.js
@@ -0,0 +1,29 @@
+const mongoose  = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Orcamento = new Schema({
+    finalizado:{
+        type: Boolean,
+        default: false
+    },
+    valor: String,
+    nome: String,
+    email: String,
+    telefone: String,
+    tipoServico: String,
+    quantidade: Number,
+    configPcb: String,
+    codigo: String,
+    urlArquivo:String,
+    processamento:[
+        {
+            status: String,
+            msg: String,
+            data: Date
+        }
+    ],
+    dataAbertura: Date,
+    dataFechamento: Date
+});
+
+mongoose.model('Orcamento', Orcamento);

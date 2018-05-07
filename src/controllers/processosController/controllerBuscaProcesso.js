775ba6ee531fea5bc61b13c70ce1e0a7bec402b3
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-07 16:41:29 -0300

    feat(src/controllers): controlador de busca de processos.

--- src/controllers/processosController/controllerBuscaProcesso.js
+++ src/controllers/processosController/controllerBuscaProcesso.js
@@ -5,5 +5,5 @@ exports.pesquisarProcesso = async (application, req, res) => {
     const codigoProcesso = req.params.id;
     const buscaProcesso = await Processo.findOne({ codigo: codigoProcesso });
     if (!buscaProcesso) return res.status(200).json({ busca: false });
-    return res.status(200).json({ busca: true, objetoBusca: buscaProcesso });
+    return res.status(200).json({ busca: true, objetoBusca: buscaProcesso});
 };

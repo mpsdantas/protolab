e0627c0213baffdd4ea40877f23dfbfb8e86f638
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-08 19:19:12 -0300

    style(/): mudanças pontuais

--- src/routes/index/index.js
+++ src/routes/index/index.js
@@ -1,3 +1,15 @@
+const path = require('path');
 module.exports = (application) => {
     application.get('/', (req, res) => { res.render('public/index') });
+    application.get('/hora', (req, res) => {
+        let data = new Date();
+        res.send(data.toLocaleString());
+    });
+    application.get('/dashboard', (req, res) => {
+        res.render('dashboard/index')
+    });
+    application.get('/get-img-perfil/:id', (req, res) => {
+        const arquivo = path.join(__dirname+'/../../uploads/images.png');
+        res.sendFile(arquivo)
+    });
 }
--- src/routes/public/authentication/authentication.js
+++ src/routes/public/authentication/authentication.js
@@ -1,3 +1,8 @@
+const authenticationController = require('../../../controllers/authenticationController');
 module.exports = (application) => {
     application.get('/login', (req, res) => { res.render('public/login') });
+    application.get('/criar-nova-conta', (req, res) => { res.render('public/novo-usuario') });
+    application.post('/verificar-email-novo-usuario', (req, res) => { authenticationController.verificarUsuario(application, req, res) });
+    application.post('/criar-novo-usuario', (req, res) => { authenticationController.criarNovoUsuario(application, req, res)});
+    
 }

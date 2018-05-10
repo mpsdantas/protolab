bcce8369d1db9d58e2fac420b9a2f7c4a6e0afaa
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-10 17:00:38 -0300

    style(src/): ajustes simples

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -4,4 +4,18 @@ module.exports = (application) => {
     application.get('/dashboard', methods.protectRoute, (req, res) => {controllerDashboard.renderIndex(application, req, res)});
     application.get('/dashboard/sair', methods.protectRoute, (req, res) => {controllerDashboard.sairDashboard(application, req, res)});
     application.get('/dashboard/ver-processo/:id', methods.protectRoute, (req,res) => { controllerDashboard.verProcesso(application, req, res) });
+    application.get('/dashboard/baixar-processo/:id', methods.protectRoute, (req,res) => { controllerDashboard.enviarArquivo(application, req, res) });
+    application.get('/dashboard/atualizar-status-processo', methods.protectRoute, (req,res) => { 
+        controllerDashboard.viewStatusProcesso(application, req, res);
+    });
+    application.get('/dashboard/realizar-atualizacao-status-processo/:id', methods.protectRoute, (req,res) => { 
+        controllerDashboard.viewRealizarStatusProcesso(application, req, res);
+    });
+    application.post('/dashboard/realizar-atualizacao-status-processo/', methods.protectRoute, (req,res) => { 
+        controllerDashboard.realizarAtualizacaoStatus(application, req, res);
+    });
+    application.get('/dashboard/obter-processos/:busca', methods.protectRoute, (req,res) => { controllerDashboard.buscarProcesso(application, req, res)});
+    application.get('/dashboard/processos-pausados', methods.protectRoute, (req,res) => { 
+        controllerDashboard.buscarProcessoPausado(application, req, res);
+    });  
 };

144c0a94bc18a2e0cf884c2a86b5404b636a386b
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-10 17:00:08 -0300

    feat(src/controllers/dashboard/): Adicionando pacotes ao insdex

--- src/controllers/dashboard/index.js
+++ src/controllers/dashboard/index.js
@@ -1,8 +1,10 @@
 const controladorPaginaInicial = require('./controladorPaginaInicial');
 const controlardorSair = require('./controladorSair');
 const controladorProcessos = require('./controladorProcessos');
+const controladorArquivos = require('./controladorArquivos');
 module.exports = {
     ...controladorPaginaInicial,
     ...controlardorSair,
-    ...controladorProcessos
+    ...controladorProcessos,
+    ...controladorArquivos
 }

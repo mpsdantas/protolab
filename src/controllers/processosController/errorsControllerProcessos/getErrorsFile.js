09de53845d980955533688578c89138b910a6d8f
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-14 18:57:04 -0300

    fix(src/controllers/dashboard/): Ajuste bug arquivos

--- src/controllers/processosController/errorsControllerProcessos/getErrorsFile.js
+++ src/controllers/processosController/errorsControllerProcessos/getErrorsFile.js
@@ -4,13 +4,16 @@ exports.getErrorsFile = (req) =>{
         return {statusErroFile:true,msg:`Você deve anexar algum arquivo`};
     }
     const sampleFile = req.files.arquivo;
+    if(!sampleFile) return {statusErroFile:true,msg:`Arquivo inválido`};
     if(req.body.tipoServico === "impressao3d"){
         if(!(path.extname(sampleFile.name)==='.stl' || path.extname(sampleFile.name)==='.STL')) return {statusErroFile:true,msg:`No processo de impressão 3d você só pode enviar arquivos STL.`};
     }
     if(req.body.tipoServico === "impressaoPCB"){
+        console.log('entrou if 2')
         if(!(path.extname(sampleFile.name)==='.zip' || path.extname(sampleFile.name)==='.gbr' || path.extname(sampleFile.name)==='.ZIP' || path.extname(sampleFile.name)==='.GBR' )) return {statusErroFile:true,msg:`No processo de impressão de PCB's você só pode enviar arquivos gerber ou zip.`};
     }
     if(!(path.extname(sampleFile.name)==='.stl' || path.extname(sampleFile.name)==='.zip' || path.extname(sampleFile.name)==='.gbr' || path.extname(sampleFile.name)==='.STL' || path.extname(sampleFile.name)==='.ZIP' || path.extname(sampleFile.name)==='.GBR')){
+        console.log('entrou if 3')
         return {statusErroFile:true,msg:`Você não pode enviar arquivos com a extensão ${path.extname(sampleFile.name)}`};
     }
     return {statusErroFile:false,msg:`Envio de arquivo autorizado.`};

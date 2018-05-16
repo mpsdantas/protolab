9234acc716974b7f0c629264c448569bdf039d57
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-16 19:04:10 -0300

    feat(src/controllers/dashboard): adicionando novas placas

--- src/controllers/dashboard/index.js
+++ src/controllers/dashboard/index.js
@@ -2,9 +2,11 @@ const controladorPaginaInicial = require('./controladorPaginaInicial');
 const controlardorSair = require('./controladorSair');
 const controladorProcessos = require('./controladorProcessos');
 const controladorArquivos = require('./controladorArquivos');
+const controladorUsuario = require('./controladorUsuario');
 module.exports = {
     ...controladorPaginaInicial,
     ...controlardorSair,
     ...controladorProcessos,
-    ...controladorArquivos
+    ...controladorArquivos,
+    ...controladorUsuario
 }

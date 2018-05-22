481a0b2cdf104d004f124fbdb7cbd67a5c38c8b8
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-22 18:36:12 -0300

    feat(src/routes/dashboard): Ajuste na rotas de orçamento

--- src/routes/dashboard/orcamentos.js
+++ src/routes/dashboard/orcamentos.js
@@ -7,6 +7,15 @@ module.exports = (application) => {
     application.get('/dashboard/realizar-atualizacao-status-orcamento/:id', methods.protectRoute, (req, res) => {
         controllerDashboard.viewRealizarStatusOrcamento(application, req, res);
     });
+    application.get('/dashboard/todos-orcamentos', methods.protectRoute, (req, res) => {
+        controllerDashboard.viewTodosOsOrcamentos(application, req, res);
+    });
+    application.get('/dashboard/obter-orcamentos-faixa-data/:dataInicial/:dataFinal', methods.protectRoute, (req, res) => {
+        controllerDashboard.buscarOrcamentosData(application, req, res);
+    });
+    application.get('/dashboard/orcamentos-pausados', methods.protectRoute, (req, res) => {
+        controllerDashboard.buscarOrcamentoPausado(application, req, res);
+    });
     application.post('/dashboard/realizar-atualizacao-status-orcamento/', methods.protectRoute, (req, res) => {
         controllerDashboard.realizarAtualizacaoStatusOrcamento(application, req, res);
     });
--- src/routes/index/index.js
+++ src/routes/index/index.js
@@ -1,5 +1,9 @@
 const methods = require('../../controllers/methods');
 module.exports = (application) => {
-    application.get('/', (req, res) => { res.render('public/index') });
+    application.get('/',  (req, res) => { 
+        if(req.session.status) res.redirect('/dashboard');
+        else res.render('public/index'); 
+        
+    });
     application.get('/get-img-perfil/:id', (req, res) => {methods.findImage(application, req, res)});
 };

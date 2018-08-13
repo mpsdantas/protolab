b61d446348e348f75e6bc778bb27920ef4cab5d0
Marcus Dantas <mpsdantas15@gmail.com>
2018-08-12 23:55:15 -0300

    Ajuste tamanho senha para 5 caracteres

--- src/controllers/authenticationController/errosUsuarioController/getErrosLogin.js
+++ src/controllers/authenticationController/errosUsuarioController/getErrosLogin.js
@@ -1,6 +1,7 @@
 exports.getErrosLogin = (req) =>{
     req.assert('email', 'O email não pode ser vazio.').notEmpty();
     req.assert('senha', 'A senha não pode ser vazia.').notEmpty();
+    req.assert('senha', 'A sua senha deve ter pelo menos 5 caracteres').isLength({ min: 5 });
     const erros = req.validationErrors();
     return erros;
 };
--- src/controllers/authenticationController/errosUsuarioController/getErrosUsuarios.js
+++ src/controllers/authenticationController/errosUsuarioController/getErrosUsuarios.js
@@ -5,6 +5,7 @@ exports.getErrosUsuarios = (req) =>{
     req.assert('tipoUsuario', 'O tipo de usuário não pode ser vazio').notEmpty();
     req.assert('repetirSenha', 'A mensagem não pode ser vazia.').notEmpty();
     req.assert('senha', 'As senhas não são iguais').equals(req.body.repetirSenha);
+    req.assert('senha', 'A sua senha deve ter pelo menos 5 caracteres').isLength({ min: 5 });
     const erros = req.validationErrors();
     return erros;
 };
